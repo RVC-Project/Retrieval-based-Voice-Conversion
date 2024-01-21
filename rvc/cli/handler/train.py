fb2960e3363a893efddea1b18e3db8c88958f27e
Ftps <ftpsflandre@gmail.com>
2024-01-21 11:59:32 +0900

    move cli

--- rvc/cli/handler/train.py
+++
@@ -1,6 +0,0 @@
-import click
-
-
-@click.command()
-def train():
-    pass
--- rvc/cli/utils/dlmodel.py
+++
@@ -1,9 +0,0 @@
-import urllib
-
-import click
-
-
-@click.command()
-def dlmodel() -> None:
-    # Download models [harvest, uvr5, and more ]
-    pass
