04149aee9700dfa258821ba2436ef155d116d3ac
alcoftTAO <alfonso.sanchez.jimenez9@gmail.com>
2024-03-06 20:29:26 +0100

    Set develop branch to default.

--- rvc/configs/config.py
+++ rvc/configs/config.py
@@ -4,6 +4,7 @@ import logging
 import os
 import sys
 from multiprocessing import cpu_count
+
 import torch
 
 try:
@@ -18,20 +19,23 @@ except (ImportError, Exception):
 
 logger: logging.Logger = logging.getLogger(__name__)
 
+
 version_config_list: list = [
-    os.path.join(root, file) for root, _, files in os.walk(os.path.dirname(os.path.abspath(__file__))) for file in files if (file.endswith(".json"))
+    os.path.join(root, file)
+    for root, dirs, files in os.walk(os.path.dirname(os.path.abspath(__file__)))
+    for file in files
+    if file.endswith(".json")
 ]
+
 
 class Config:
     def __new__(cls):
-        if (not hasattr(cls, "_instance")):
+        if not hasattr(cls, "_instance"):
             cls._instance = super().__new__(cls)
-        
         return cls._instance
 
-    def __init__(self) -> None:
+    def __init__(self):
         self.device: str = "cuda:0"
-        self.use_gpu: bool = torch.cuda.is_available()
         self.is_half: bool = True
         self.use_jit: bool = False
         self.n_cpu: int = cpu_count()
@@ -39,34 +43,63 @@ class Config:
         self.json_config = self.load_config_json()
         self.gpu_mem: int | None = None
         self.instead: str | None = None
-        (self.python_cmd, self.listen_port, self.noparallel, self.noautoopen, self.dml) = self.arg_parse()
+        (
+            self.python_cmd,
+            self.listen_port,
+            self.noparallel,
+            self.noautoopen,
+            self.dml,
+        ) = self.arg_parse()
         self.x_pad, self.x_query, self.x_center, self.x_max = self.device_config()
 
     @staticmethod
     def load_config_json() -> dict:
         return {
-            config_file: json.load(open(config_file, "r")) for config_file in version_config_list
+            config_file: json.load(open(config_file, "r"))
+            for config_file in version_config_list
         }
 
     @staticmethod
     def arg_parse() -> tuple:
         parser: argparse.ArgumentParser = argparse.ArgumentParser()
-
-        parser.add_argument("--port", type = int, default=7865, help = "Listen port")
-        parser.add_argument("--pycmd", type = str, default = sys.executable or "python", help = "Python command")
-        parser.add_argument("--noparallel", action = "store_true", help = "Disable parallel processing")
-        parser.add_argument("--noautoopen", action = "store_true", help = "Do not open in browser automatically")
-        parser.add_argument("--dml", action = "store_true", help = "torch_dml")
-
+        parser.add_argument("--port", type=int, default=7865, help="Listen port")
+        parser.add_argument(
+            "--pycmd",
+            type=str,
+            default=sys.executable or "python",
+            help="Python command",
+        )
+        parser.add_argument(
+            "--noparallel", action="store_true", help="Disable parallel processing"
+        )
+        parser.add_argument(
+            "--noautoopen",
+            action="store_true",
+            help="Do not open in browser automatically",
+        )
+        parser.add_argument(
+            "--dml",
+            action="store_true",
+            help="torch_dml",
+        )
         cmd_opts: argparse.Namespace
         cmd_opts, _ = parser.parse_known_args()
 
         cmd_opts.port = cmd_opts.port if 0 <= cmd_opts.port <= 65535 else 7865
-        return (cmd_opts.pycmd, cmd_opts.port, cmd_opts.noparallel, cmd_opts.noautoopen, cmd_opts.dml)
+
+        return (
+            cmd_opts.pycmd,
+            cmd_opts.port,
+            cmd_opts.noparallel,
+            cmd_opts.noautoopen,
+            cmd_opts.dml,
+        )
 
     @staticmethod
     def has_mps() -> bool:
-        return torch.backends.mps.is_available() and not torch.zeros(1).to(torch.device("mps"))
+        return torch.backends.mps.is_available() and not torch.zeros(1).to(
+            torch.device("mps")
+        )
 
     @staticmethod
     def has_xpu() -> bool:
@@ -76,59 +109,62 @@ class Config:
         for config_file, data in self.json_config.items():
             try:
                 data["train"]["fp16_run"] = False
-
                 with open(config_file, "w") as json_file:
                     json.dump(data, json_file, indent=4)
             except Exception as e:
                 logger.info(f"Error updating {config_file}: {str(e)}")
-        
         logger.info("overwrite configs.json")
 
     def device_config(self) -> tuple:
-        if (torch.cuda.is_available() and self.use_gpu):
-            if (self.has_xpu()):
+        if torch.cuda.is_available():
+            if self.has_xpu():
                 self.device = self.instead = "xpu:0"
                 self.is_half = True
-            
             i_device = int(self.device.split(":")[-1])
             self.gpu_name = torch.cuda.get_device_name(i_device)
-
-            if (("16" in self.gpu_name and "V100" not in self.gpu_name.upper()) or "P40" in self.gpu_name.upper() or "P10" in self.gpu_name.upper() or "1060" in self.gpu_name or "1070" in self.gpu_name or "1080" in self.gpu_name):
+            if (
+                ("16" in self.gpu_name and "V100" not in self.gpu_name.upper())
+                or "P40" in self.gpu_name.upper()
+                or "P10" in self.gpu_name.upper()
+                or "1060" in self.gpu_name
+                or "1070" in self.gpu_name
+                or "1080" in self.gpu_name
+            ):
                 logger.info(f"Found GPU {self.gpu_name}, force to fp32")
-
                 self.is_half = False
                 self.use_fp32_config()
             else:
                 logger.info(f"Found GPU {self.gpu_name}")
-            
-            self.gpu_mem = int(torch.cuda.get_device_properties(i_device).total_memory / 1024 / 1024 / 1024 + 0.4)
-        elif (self.has_mps()):
-            logger.info("No supported Nvidia GPU found, using mps instead")
-
+            self.gpu_mem = int(
+                torch.cuda.get_device_properties(i_device).total_memory
+                / 1024
+                / 1024
+                / 1024
+                + 0.4
+            )
+        elif self.has_mps():
+            logger.info("No supported Nvidia GPU found")
             self.device = self.instead = "mps"
             self.is_half = False
-
             self.use_fp32_config()
-        elif (self.dml):
+        elif self.dml:
             import torch_directml
 
             self.device = torch_directml.device(torch_directml.default_device())
             self.is_half = False
         else:
-            logger.info("No supported Nvidia GPU found, using cpu instead")
-
+            logger.info("No supported Nvidia GPU found")
             self.device = self.instead = "cpu"
             self.is_half = False
-
             self.use_fp32_config()
 
-        if (self.gpu_mem != None and self.gpu_mem <= 4):
+        if self.gpu_mem is not None and self.gpu_mem <= 4:
             x_pad = 1
             x_query = 5
             x_center = 30
             x_max = 32
-        elif (self.is_half):
-            # 6G GPU_RAM conf
+        elif self.is_half:
+            # 6G PU_RAM conf
             x_pad = 3
             x_query = 10
             x_center = 60
@@ -142,5 +178,4 @@ class Config:
 
         logger.info(f"Use {self.dml or self.instead} instead")
         logger.info(f"is_half:{self.is_half}, device:{self.device}")
-
         return x_pad, x_query, x_center, x_max
