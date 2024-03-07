0e8cf4d0f4d192ee462521bd0b4e8732bae4eca6
Ftps <ftpsflandre@gmail.com>
2024-03-07 22:40:52 +0900

    jit

--- rvc/lib/jit/get_hubert.py
+++ rvc/lib/jit/get_hubert.py
@@ -1,10 +1,11 @@
 import math
 import random
 from typing import Optional, Tuple
-from fairseq.checkpoint_utils import load_model_ensemble_and_task
+
 import numpy as np
 import torch
 import torch.nn.functional as F
+from fairseq.checkpoint_utils import load_model_ensemble_and_task
 
 # from fairseq.data.data_utils import compute_mask_indices
 from fairseq.utils import index_put
